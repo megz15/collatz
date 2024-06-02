32dcf1121b93f652df88bc043da61a45ad59b938
megz15 <meghraj.g16@gmail.com>
2024-06-03 03:26:45 +0530

    chore: cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     import { LineChart } from 'chartist'
 	import { onMount } from 'svelte'
-	import svelteLogo from './assets/svelte.svg'
-	import viteLogo from '/vite.svg'
 
 	let n:number
 	let collatz_sequence: number[] = []
@@ -36,14 +34,6 @@
 </script>
 
 <main>
-	<div>
-		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-		<img src={viteLogo} class="logo" alt="Vite Logo" />
-		</a>
-		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
-		<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-		</a>
-	</div>
 	<h1>Collatz Conjecture</h1>
 
 	<div class="card">
@@ -72,18 +62,3 @@
 	</div>
 
 </main>
-
-<style>
-	.logo {
-		height: 6em;
-		padding: 1.5em;
-		will-change: filter;
-		transition: filter 300ms;
-	}
-	.logo:hover {
-		filter: drop-shadow(0 0 2em #646cffaa);
-	}
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #ff3e00aa);
-	}
-</style>
