d8cf6ed746b3741e97846ea71454680c7d6acb90
megz15 <meghraj.g16@gmail.com>
2024-06-03 12:29:52 +0530

    feat: switch to chart.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-    import { LineChart } from 'chartist'
-	import { onMount } from 'svelte'
+	import { onMount } from 'svelte';
+    import { Chart } from 'chart.js/auto';
 
 	let n:number
 	let collatz_sequence: number[] = []
-	let chart: LineChart;
 
-    const collatz = () => {
+	let chart: Chart;
+  	let chartCanvas: HTMLCanvasElement;
+
+    const calculate = () => {
 		collatz_sequence = [n]
 		while (n!=1) {			
 			if (n%2 == 0) n/=2
@@ -14,22 +16,55 @@
 			collatz_sequence.push(n)
 		}
 		n = collatz_sequence[0]
-
-		chart.update({
-			labels: collatz_sequence.map((_, i) => i+1),
-			series: [collatz_sequence]
-		})
+		updateChart()
 	}
 
 	onMount(() => {
-		chart = new LineChart(
-			'#chart',
+		chart = new Chart(chartCanvas, {
+		type: 'line',
+		data: {
+			labels: [],
+			datasets: [
 			{
-				labels: [],
-				series: [[]]
+				label: 'Collatz Sequence Value',
+				data: [],
+				tension: 0.2
 			}
-		)
-	})
+			]
+		},
+		options: {
+			plugins: {
+				legend: {
+					display: false
+				},
+			},
+			scales: {
+			x: {
+				beginAtZero: true,
+				title: {
+				display: true,
+				text: 'Index'
+				}
+			},
+			y: {
+				beginAtZero: true,
+				title: {
+				display: true,
+				text: 'Value'
+				}
+			}
+			}
+		}
+		});
+	});
+
+	const updateChart = () => {
+		if (chart) {
+		chart.data.labels = collatz_sequence.map((_, index) => index + 1);
+		chart.data.datasets[0].data = collatz_sequence;
+		chart.update();
+		}
+	};
 
 </script>
 
@@ -37,10 +72,12 @@
 	<h1>Collatz Conjecture</h1>
 
 	<div class="card">
-		Numero: <input placeholder="Enter value" bind:value={n}> <button on:click={collatz}>Enter</button>
+		<input placeholder="Value" bind:value={n} size="10"> <button on:click={calculate}>Enter</button>
 	</div>
 
-	<div id="chart"></div>
+	<div class="card">
+		<canvas bind:this={chartCanvas} />
+	</div>
 
 	<div class="card">
 		<table>
@@ -57,8 +94,8 @@
 		</table>
 	</div>
 
-	<div class="card">
+	<footer>
 		Made with &lt;3 by <a href="https://github.com/megz15" target="_blank" rel="noreferrer">Meghraj Goswami</a>
-	</div>
+	</footer>
 
 </main>
